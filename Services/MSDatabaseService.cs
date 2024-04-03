4b54eb684fe16c5412200378e8b2b4ffd3d08d8a
sam <samuelpospisil1@gmail.com>
2024-04-03 15:03:16 +0200

    init

---
+++ Services/MSDatabaseService.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RDB_Mytne.Services
+{
+    public class MSDatabaseService
+    {
+        private SqlConnection connection;
+        public MSDatabaseService() {
+            connection = new SqlConnection(
+               "user id=sam;" +
+               "password=rdb.1234;" +
+               "server=rdb-mytne.database.windows.net;" +
+               "Trusted_Connection=False;" +
+               "database=rdb-mytne; " +
+               "connection timeout=30");
+        }
+
+        public string sendSQLCommand(string sqlCommand)
+        {
+            try
+            {
+                connection.Open();
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception.ToString());
+            }
+
+            // Create SQL command
+            SqlCommand command = new SqlCommand(sqlCommand, connection);
+            command.ExecuteNonQuery();
+
+
+            // Get output
+            StringBuilder output = new();
+            try
+            {
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    
+                    while (reader.Read())
+                    {
+                        output.Append(reader[0].ToString() + ", ");
+                        output.Append(reader[1].ToString() + ", ");
+                        output.Append(reader[2].ToString() + Environment.NewLine);
+                    }
+                }
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception.ToString());
+            }
+
+            // Close connection
+            try
+            {
+                connection.Close();
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception.ToString());
+            }
+
+            return output.ToString();
+        }
+    }
+}
---
+++ Services/MongoDBService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Driver;
+using MongoDB.Bson;
+
+namespace RDB_Mytne.Services
+{
+    public class MongoDBService
+    {
+        MongoClient client;
+        public MongoDBService()
+        {
+            string connectionString = "mongodb+srv://samuelpospisil:qSWsQlzDWNZxumyl@rdb-mytne-mongo.nncfcdc.mongodb.net/?retryWrites=true&w=majority&appName=rdb-mytne-mongo";
+            client = new(connectionString);
+
+            try
+            {
+                var collection = client.GetDatabase("sample_mflix").GetCollection<BsonDocument>("movies");
+                var filter = Builders<BsonDocument>.Filter.Eq("title", "Back to the Future");
+                var document = collection.Find(filter).First();
+                Console.WriteLine(document);
+            }
+            catch(Exception exception)
+            {
+                Console.WriteLine(exception.ToString());
+            }
+
+        }
+    }
+}
